b5f4831f2fdfd237a8088f58158b910c1155d1a1
shriya731 <68431517+shriya731@users.noreply.github.com>
2020-11-08 00:22:11 +0530

    Main App class

---
+++ src/main/java/com/java/atm/atm_machine/App.java
@@ -0,0 +1,12 @@
+package com.java.atm.atm_machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
